5152293086382f4d4571533b4c92e2b643788189
darksun <lujun9972@sina.com>
2016-03-01 17:13:19 +0800

    version1

--- hackertyper.el
+++ hackertyper.el
@@ -1,33 +1,32 @@
+(defcustom hackertyper-buffer "*hackertyper*"
+  "")
+
 (defvar-local hackertyper-content ""
   "zb时输入的文本")
 
-(defun hackertyper-self-insert-command-advise(&optional N)
+(defun hackertyper-self-insert-command(&optional N)
   ""
   (interactive "P")
   (if (string= hackertyper-content "")
-	  (progn 
-		(hackertyper-disable)
-		(self-insert-command (or N 1)))
+	  (self-insert-command (or N 1))
 	(insert (elt hackertyper-content 0))
 	(setq hackertyper-content (substring hackertyper-content 1))))
 
 ;;;###autoload
-(defun hackertyper-enable ()
-  "开启zb模式"
-  (interactive)
-  (when (string= hackertyper-content "")
-	(if (= (point) (point-max))
-		(progn
-		  (require 'file-helper)
-		  (setq hackertyper-content (file-content (read-file-name "从哪个文件导入装逼的内容?"))))
-	  (setq hackertyper-content (buffer-substring-no-properties (point) (point-max)))
-	  (delete-region (point) (point-max))))
-  (advice-add 'self-insert-command :override #'hackertyper-self-insert-command-advise))
+(define-derived-mode hackertyper-mode fundamental-mode "hackertyper"
+  "Major mode for running hacker typer"
+  (local-set-key  [remap self-insert-command] 'hackertyper-self-insert-command)
+  )
 
 ;;;###autoload
-(defun hackertyper-disable ()
-  "关闭zb模式"
+(defun hackertyper ()
+  "开启zb模式"
   (interactive)
-  (advice-remove 'self-insert-command #'hackertyper-self-insert-command-advise))
+  (switch-to-buffer (get-buffer-create hackertyper-buffer))
+  (erase-buffer)
+  (hackertyper-mode)
+  (setq hackertyper-content (with-temp-buffer
+                              (insert-file-contents  (read-file-name "从哪个文件导入装逼的内容?"))
+                              (buffer-substring (point-min) (point-max)))))
 
 (provide 'hackertyper)
